23ff5a07a7e443ce81d8cd1f0a651af77db056fa
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-31 10:18:17 +0000

    Deploying to gh-pages from @ Pugli87/goit-js-hw-10@2118fc1542fbda0991e321c2250257d84c597395 🚀

--- index.1999d6d5.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".error"),t=document.querySelector(".loader");e.setAttribute("hidden",!0),t.setAttribute("hidden",!0);
-//# sourceMappingURL=index.1999d6d5.js.map
--- index.4cf4b4f7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".error"),t=document.querySelector(".loader");e.setAttribute("hidden",!0),t.setAttribute("hidden",!0)}();
-//# sourceMappingURL=index.4cf4b4f7.js.map
