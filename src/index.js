02df1f805f86fe1c2ece118672bb84ad45b7149b
Pugli87 <119467014+Pugli87@users.noreply.github.com>
2023-05-31 04:30:50 -0500

    archivo index.js

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// keys the Cat API
+//live_6xQUElFdFTDJFzptsVrEfgwlEfCFOu0dcC3VKzRePi1XDOiI9r62RJ6LiRGtC5sW
+const errorPage = document.querySelector(".error");
+const loaderPage = document.querySelector(".loader");
+errorPage.setAttribute("hidden", true);
+loaderPage.setAttribute("hidden", true);
